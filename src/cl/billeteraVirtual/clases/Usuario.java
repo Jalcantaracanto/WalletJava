49869e6aab6f366717c93f2f5b27a02c2f4195c1
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2024-03-20 20:07:49 -0300

    Clase Usuario
    Se completa clase usuario.
    Solo falta método listarUsuario, que esta en revisión

--- src/cl/billeteraVirtual/clases/Usuario.java
+++ src/cl/billeteraVirtual/clases/Usuario.java
@@ -3,6 +3,7 @@ package cl.billeteraVirtual.clases;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Usuario {
     // 1 - Atributos
@@ -26,6 +27,13 @@ public class Usuario {
     //Métodos Custom
 
     public Usuario crearUsuario() {
+
+        UUID uuid = UUID.randomUUID();
+        long mostSignificantBits = uuid.getMostSignificantBits();
+        long leastSignificantBits = uuid.getLeastSignificantBits();
+        long id = mostSignificantBits ^ leastSignificantBits;
+
+
         Scanner scanner = new Scanner(System.in);
         String nombre, apellido, rut, correo, contrasena, vContrasena;
 
@@ -52,7 +60,7 @@ public class Usuario {
             if (validarCorreo(correo)) {
                 break;
             } else {
-                menu.mensajeCorreoInvalido();
+                Menu.mensajeCorreoInvalido();
             }
         }
 
@@ -64,15 +72,72 @@ public class Usuario {
             if (contrasena.equals(vContrasena)) {
                 break;
             } else {
-                menu.mensajeContrasenaDistinta();
+                Menu.mensajeContrasenaDistinta();
             }
         }
 
-        return new Usuario(nombre, apellido, rut, correo, contrasena);
+        return new Usuario(id, nombre, apellido, rut, correo, contrasena);
     }
 
     public void listarUsuario() {
 
+    }
+
+    public boolean validarRut(String rut) {
+
+        boolean validacion = false;
+        try {
+            rut = rut.toUpperCase();
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+            char dv = rut.charAt(rut.length() - 1);
+
+            int m = 0, s = 1;
+            for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+            }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        return validacion;
+    }
+
+    /**
+     * @param rut Se rescata el Rut ingresado por el usuario.
+     * @return rut completo formateado con puntos y guíon.
+     */
+    public String formatearRut(String rut) {
+        int cont = 0;
+        StringBuilder format;
+        if (rut.isEmpty()) {
+            return "";
+        } else {
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            format = new StringBuilder(("-" + rut.substring(rut.length() - 1)));
+            for (int i = rut.length() - 2; i >= 0; i--) {
+                format.insert(0, rut.charAt(i));
+                cont++;
+                if (cont == 3 && i != 0) {
+                    format.insert(0, ".");
+                    cont = 0;
+                }
+            }
+            return format.toString();
+        }
+    }
+
+
+    public boolean validarCorreo(String correo) {
+        // Expresión regular simple para validar un correo electrónico
+        String regex = "^[^@]+@[^@]+\\.[a-zA-Z]{2,}$";
+        return correo.matches(regex);
     }
 
 }
